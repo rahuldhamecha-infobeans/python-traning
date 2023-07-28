566c5b492ab0f352711dc2262c0860ba7c279195
rahuldhamecha <rahul.dhamecha@infobeans.com>
2023-07-28 14:52:41 +0530

    Django Model Forms

--- django/first_project/user_app/urls.py
+++ django/first_project/user_app/urls.py
@@ -3,4 +3,5 @@ from user_app import views
 
 urlpatterns = [
     re_path(r'^$',views.index,name='user_index'),
+    path('forms/',views.register,name='user_registration')
 ]
--- django/first_project/user_app/views.py
+++ django/first_project/user_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from user_app.models import User
+from user_app.forms import UserForm
 # Create your views here.
 
 def index(request):
-    users = User.objects.order_by('first_name')
+    users = User.objects.order_by('id')
     user_content = {'users' : users}
     return render(request,'user_app/index.html',user_content)
+
+def register(request):
+    form = UserForm()
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM INVALID")
+
+    return render(request,'user_app/forms.html',{'form':form})
