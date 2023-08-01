7cc2b09b1e3a05f6bd6a65a7a3242b7e75878421
rahuldhamecha <rahul.dhamecha@infobeans.com>
2023-08-01 10:37:44 +0530

    Django Advanced  List,DetailView

--- django/first_project/advanced_app/urls.py
+++ django/first_project/advanced_app/urls.py
@@ -5,4 +5,7 @@ app_name = "advanced"
 
 urlpatterns = [
     re_path(r'^$',views.IndexView.as_view(),name='index'),
+    path('schools/',views.SchoolListView.as_view(),name='school'),
+    path('students/',views.SchoolListView.as_view(),name='student'),
+    re_path(r"^schools/(?P<pk>\w+)/$", views.SchoolDetailView.as_view(),name='school_detail'),
 ]
--- django/first_project/advanced_app/views.py
+++ django/first_project/advanced_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
-
+from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'advanced_app/index.html'
@@ -9,3 +9,14 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['page_name'] = 'Advanced Page'
         return context
+
+class SchoolListView(ListView):
+    template_name = 'advanced_app/school_list.html'
+    # by default school_list variable return in ListView (MODELNAME_list)
+    context_object_name = 'schools'
+    model = models.School
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'advanced_app/details.html'
